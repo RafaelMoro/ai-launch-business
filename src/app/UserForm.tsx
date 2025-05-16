d4d66c250a1c892441db2bdcb4c0f07fc72bfe4c
Rafael Moro G <rafael.moro.galindo@gmail.com>
2025-05-15 22:41:44 -0600

    add handle submit fn

--- src/app/UserForm.tsx
+++ src/app/UserForm.tsx
@@ -1,6 +1,13 @@
+'use client'
+import { FormEvent } from "react"
+
 export default function UserForm() {
+  const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault()
+    console.log('submitted')
+  }
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <label
         htmlFor="message"
         className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
@@ -10,7 +17,7 @@ export default function UserForm() {
         className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
         placeholder="Ingrese su idea de negocio"></textarea>
 
-      <button type="button" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Obtener asesoria</button>
+      <button type="submit" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Obtener asesoria</button>
     </form>
   )
 }
