05e9f686755081ac9cc52f1b8023c4a3e49bc647
Rafael Moro G <rafael.moro.galindo@gmail.com>
2025-05-16 14:54:58 -0600

    style better the card of show competitors

--- src/app/components/CompetitorsInfo.tsx
+++ src/app/components/CompetitorsInfo.tsx
@@ -115,7 +115,7 @@ export default function CompetitorsInfo({
         </>
       ) }
       { competitors.length > 0 && (
-        <div className="grid grid-cols-3 gap-4">
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
           { competitors.map((competitor) => (
           <ShowCompetitors key={competitor.name} competitor={competitor} />
         ))}
