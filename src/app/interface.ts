0299fd53c8c75f25a27ac77fed0f232d97235cd4
Rafael Moro G <rafael.moro.galindo@gmail.com>
2025-05-16 13:02:47 -0600

    add local storage interfaces

--- src/app/interface.ts
+++ src/app/interface.ts
@@ -1,8 +1,22 @@
 export type GeoPosition = 'granted' | 'denied' | 'prompt';
 
+export interface GeoLocationCoords {
+  latitude: number | null;
+  longitude: number | null;
+}
+
+export interface Emprende25LocalStorage {
+  geoLocationCoords: GeoLocationCoords;
+}
+
 export interface GetBusinessPlanData {
   businessStrategy: string;
   inversionPlan: string;
   businessGoals: string;
   possibleRisks: string;
+}
+
+export interface GeolocationInfo {
+  state: string;
+  country: string;
 }
