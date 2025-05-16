13d8f1c7681a02b51f8cb7728fd7f8726b1f212f
Rafael Moro G <rafael.moro.galindo@gmail.com>
2025-05-16 09:32:10 -0600

    add show business plan comp into main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ import { useState } from "react";
 import GetPosition from "./GetPosition";
 import UserForm from "./UserForm";
 import { GetBusinessPlanData } from "./interface";
+import ShowBusinessPlan from "./components/ShowBusinessPlan";
 
 export default function Home() {
   const [businessIdea, setBusinessIdea] = useState("")
@@ -28,15 +29,7 @@ export default function Home() {
         resetBusinessIdea={resetBusinessIdea}
         />
       { businessPlan && (
-        <div>
-          <h2 className="text-2xl font-bold">Asesoría de negocio</h2>
-          <div className="grid grid-cols-1 gap-4">
-            <p><strong>Estrategia de negocio:</strong> {businessPlan.businessStrategy}</p>
-            <p><strong>Plan de inversión:</strong> {businessPlan.inversionPlan}</p>
-            <p><strong>Objetivos de negocio:</strong> {businessPlan.businessGoals}</p>
-            <p><strong>Posibles riesgos:</strong> {businessPlan.possibleRisks}</p>
-          </div>
-        </div>
+        <ShowBusinessPlan businessPlan={businessPlan} />
       )}
     </div>
   );
