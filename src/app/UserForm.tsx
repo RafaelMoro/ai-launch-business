6daa2a608959a20f45dfcb65e3ff8cc02216943a
Rafael Moro G <rafael.moro.galindo@gmail.com>
2025-05-16 09:59:20 -0600

    refactor: add isIdle state to user form mutation and improve button state handling

--- src/app/UserForm.tsx
+++ src/app/UserForm.tsx
@@ -15,7 +15,7 @@ interface UserFormProps {
 
 export default function UserForm({ businessIdea, addBusinessPlan, resetBusinessIdea, updateBusinessIdea }: UserFormProps) {
   const [buttonText, setButtonText] = useState('Obtener asesoria')
-  const { mutate: getBusinessPlan, isError, isPending, isSuccess } = useMutation({
+  const { mutate: getBusinessPlan, isError, isPending, isSuccess, isIdle } = useMutation({
     mutationFn: () => {
       const data = { businessIdea }
       return axios.post(`${backendUri}/business-plan`, data)
@@ -32,19 +32,25 @@ export default function UserForm({ businessIdea, addBusinessPlan, resetBusinessI
   }
 
   return (
-    <form onSubmit={handleSubmit}>
-      <label
-        htmlFor="message"
-        className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-        >Ingrese su idea de su negocio de manera breve y concisa</label>
-      <textarea
-        id="message"
-        value={businessIdea}
-        onChange={(e) => updateBusinessIdea(e.target.value)}
-        className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-        placeholder="Ingrese su idea de negocio"></textarea>
+    <>
+      <form onSubmit={handleSubmit}>
+        <label
+          htmlFor="message"
+          className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+          >Ingrese su idea de su negocio de manera breve y concisa</label>
+        <textarea
+          id="message"
+          value={businessIdea}
+          onChange={(e) => updateBusinessIdea(e.target.value)}
+          className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+          placeholder="Ingrese su idea de negocio"></textarea>
 
-      <button type="submit" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Obtener asesoria</button>
-    </form>
+        <button disabled={isPending || isSuccess} type="submit" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">
+          { (isIdle || isSuccess) && 'Obtener asesoria' }
+          { isPending && 'Cargando...'}
+        </button>
+      </form>
+      { isError && (<p>Oops! Parece que hubo un error. Por favor intente más tarde</p>) }
+    </>
   )
 }
