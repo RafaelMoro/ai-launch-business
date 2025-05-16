09a8d16835aaff5ae647686a749a69bd3c769e5b
Rafael Moro G <rafael.moro.galindo@gmail.com>
2025-05-15 22:38:28 -0600

    install flowbite

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import GetPosition from "./GetPosition";
+import UserForm from "./UserForm";
 
 export default function Home() {
 
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <GetPosition />
+      <UserForm />
     </div>
   );
 }
