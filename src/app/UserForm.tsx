71b915474442842f5e627ae7988e345ca77fbdf1
Rafael Moro G <rafael.moro.galindo@gmail.com>
2025-05-15 22:29:02 -0600

    add user form

---
+++ src/app/UserForm.tsx
@@ -0,0 +1,8 @@
+export default function UserForm() {
+  return (
+    <form action="">
+      <input type="text" placeholder="Ingrese su idea de negocio de manera concisa y breve" />
+      <button>Obtener asesoría</button>
+    </form>
+  )
+}
