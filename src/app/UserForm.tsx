253cb8f01cc8585997541114819c4f88d14655ac
Rafael Moro G <rafael.moro.galindo@gmail.com>
2025-05-16 10:06:56 -0600

    refactor: clean up UserForm component by removing unused state and improving button structure

--- src/app/UserForm.tsx
+++ src/app/UserForm.tsx
@@ -1,5 +1,5 @@
 'use client'
-import { FormEvent, useState } from "react"
+import { FormEvent } from "react"
 import axios from 'axios'
 import { useMutation } from "@tanstack/react-query"
 
@@ -14,7 +14,6 @@ interface UserFormProps {
 }
 
 export default function UserForm({ businessIdea, addBusinessPlan, resetBusinessIdea, updateBusinessIdea }: UserFormProps) {
-  const [buttonText, setButtonText] = useState('Obtener asesoria')
   const { mutate: getBusinessPlan, isError, isPending, isSuccess, isIdle } = useMutation({
     mutationFn: () => {
       const data = { businessIdea }
@@ -26,6 +25,7 @@ export default function UserForm({ businessIdea, addBusinessPlan, resetBusinessI
       resetBusinessIdea() // Clear form after successful submission
     }
   })
+
   const handleSubmit = async (event: FormEvent<HTMLFormElement>) => {
     event.preventDefault()
     getBusinessPlan()
@@ -43,9 +43,14 @@ export default function UserForm({ businessIdea, addBusinessPlan, resetBusinessI
           value={businessIdea}
           onChange={(e) => updateBusinessIdea(e.target.value)}
           className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-          placeholder="Ingrese su idea de negocio"></textarea>
+          placeholder="Ingrese su idea de negocio"
+          ></textarea>
 
-        <button disabled={isPending || isSuccess} type="submit" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">
+        <button
+          disabled={isPending || isSuccess}
+          type="submit"
+          className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800 disabled:opacity-50"
+          >
           { (isIdle || isSuccess) && 'Obtener asesoria' }
           { isPending && 'Cargando...'}
         </button>
