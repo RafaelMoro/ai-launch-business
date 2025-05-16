47aa673e88952c62a995279427e5d479eae5a129
Rafael Moro G <rafael.moro.galindo@gmail.com>
2025-05-16 09:53:36 -0600

    add initial config of tanstack query into main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,16 @@
 'use client'
 import { useState } from "react";
+import {
+  QueryClient,
+  QueryClientProvider,
+} from '@tanstack/react-query'
+
 import GetPosition from "./GetPosition";
 import UserForm from "./UserForm";
 import { GetBusinessPlanData } from "./interface";
 import ShowBusinessPlan from "./components/ShowBusinessPlan";
+
+const queryClient = new QueryClient()
 
 export default function Home() {
   const [businessIdea, setBusinessIdea] = useState("")
@@ -20,17 +27,19 @@ export default function Home() {
   const resetBusinessIdea = () => setBusinessIdea("");
 
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <GetPosition addLatitude={addLatitude} addLongitude={addLongitude} />
-      <UserForm
-        addBusinessPlan={addBusinessPlan}
-        businessIdea={businessIdea}
-        updateBusinessIdea={updateBusinessIdea}
-        resetBusinessIdea={resetBusinessIdea}
-        />
-      { businessPlan && (
-        <ShowBusinessPlan businessPlan={businessPlan} />
-      )}
-    </div>
+    <QueryClientProvider client={queryClient}>
+      <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+        <GetPosition addLatitude={addLatitude} addLongitude={addLongitude} />
+        <UserForm
+          addBusinessPlan={addBusinessPlan}
+          businessIdea={businessIdea}
+          updateBusinessIdea={updateBusinessIdea}
+          resetBusinessIdea={resetBusinessIdea}
+          />
+        { businessPlan && (
+          <ShowBusinessPlan businessPlan={businessPlan} />
+        )}
+      </div>
+    </QueryClientProvider>
   );
 }
