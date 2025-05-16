97e6c472d107140c1ea8b056c798e4ae8dad4c60
Rafael Moro G <rafael.moro.galindo@gmail.com>
2025-05-15 22:38:42 -0600

    change user form using flowbite

--- src/app/UserForm.tsx
+++ src/app/UserForm.tsx
@@ -1,8 +1,16 @@
 export default function UserForm() {
   return (
-    <form action="">
-      <input type="text" placeholder="Ingrese su idea de negocio de manera concisa y breve" />
-      <button>Obtener asesoría</button>
+    <form>
+      <label
+        htmlFor="message"
+        className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+        >Ingrese su idea de su negocio de manera breve y concisa</label>
+      <textarea
+        id="message"
+        className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+        placeholder="Ingrese su idea de negocio"></textarea>
+
+      <button type="button" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Obtener asesoria</button>
     </form>
   )
 }
